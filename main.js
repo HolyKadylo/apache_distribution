5ae3f109372ba699dbb483f53db17942b350073c
Illya Piven <pivas47@gmail.com>
2020-02-25 03:57:54 +0200

    finished working on distributing by difficulty

--- main.js
+++ main.js
@@ -30,11 +30,11 @@ function constructTable(selector) {
 	// distributes the current people on the shift	
 	// sorted contains actual commands	
 	//var sorted = distribute(unsorted, smelist)
-	var sorted = []
-	sorted[0] = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"]
-	sorted[1] = ["1","2","3","4","5","6","7","8","9","10","11","12"]
-	sorted[2] = ["1","2","3","4","5","6","7","8"]
-	console.log(sorted.length)
+	//var sorted = []
+	//sorted[0] = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"]
+	//sorted[1] = ["1","2","3","4","5","6","7","8","9","10","11","12"]
+	//sorted[2] = ["1","2","3","4","5","6","7","8"]
+	//console.log(sorted.length)
 
 	// creating SME row
 	for (var i = 0; i < smelist.length; i++){
@@ -53,18 +53,54 @@ function constructTable(selector) {
 // returns an array that can be easily distributed among SMEs
 function distribute(unsorted, smelist){
 	skills = JSON.parse(skillsRaw);
-	console.log ("helllo")
-	var diffcount[]
-	var ratecount[]
-	
+	var diffcount = []
+	var ratecount = []	
 	var sorted = []
-	for (var i = 0; i <= unsorted.length; i++){
-		console.log(unsorted["name"])
 
+	// initializing
+	for (var i = 0; i < smelist.length; i++){
+		sorted[i] = [" ", ": "]
+		diffcount[i] = 0
+		ratecount[i] = 0
 	}
-		
 
-
-
+	// working on addition
+	for (var i = 0; i < unsorted.length; i++){
+			
+		//adding by difficulty to the SME that has the lower total diff
+		var mindiff = Array.min(diffcount)
+		var minrate = Array.min(ratecount)
+		for (var j = 0; j < smelist.length; j++){
+			
+			if (diffcount[j] == mindiff){
+				
+				// adding to resulting array
+				sorted[j].push(unsorted[i]["name"]);
+				var pushedName = unsorted[i]["name"]
+				
+				// finding the skill
+				var skill = ""
+				for (var k = 0; k < unsorted.length; k++){
+					if (pushedName.localeCompare(skills["people"][k]["name"]) == 0){
+						skill = skills["people"][k]["skill"]
+					}						
+				}
+				
+				diffcount[j] += parseInt(skills["diff-coeffs"][skill])
+				ratecount[j] += parseInt(skills["rate-coeffs"][skill])
+				break;
+			}	
+		}
+	}
 	return sorted;
 }
+
+
+//https://stackoverflow.com/questions/8934877/obtain-smallest-value-from-array-in-javascript
+// returns smallest value in array
+Array.min = function( array ){
+    return Math.min.apply( Math, array );
+};
+
+
+
