9144434ed9134cee9f6e31470f1b3a5a13005232
Illya Piven <pivas47@gmail.com>
2020-03-18 15:06:30 +0200

    +python issues

--- issueTracker.js
+++ issueTracker.js
@@ -1,8 +1,17 @@
 var urlToGetAllOpenBugs = "https://api.github.com/repos/HolyKadylo/apache_distribution/issues?state=open";
+var urlToGetAllOpenBugs2 = "https://api.github.com/repos/HolyKadylo/python_distribution/issues?state=open";
 
 $(document).ready(function () {
     $.getJSON(urlToGetAllOpenBugs, function (allIssues) {
         $("div").append("################</br>");
+        $.each(allIssues, function (i, issue) {
+            $("div")
+                .append("<b>" + " - " + issue.title + "</b></br>")
+                .append("created at: " + issue.created_at + "</br>")
+                .append(issue.body + "</br>");
+        });
+    });
+    $.getJSON(urlToGetAllOpenBugs2, function (allIssues) {
         $.each(allIssues, function (i, issue) {
             $("div")
                 .append("<b>" + " - " + issue.title + "</b></br>")
